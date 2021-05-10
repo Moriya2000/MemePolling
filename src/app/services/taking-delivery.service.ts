69d3cc55e5eb1928a459bd3bd1d08a9a77604edd
moriya <moriyash149@gmail.com>
2021-05-10 03:55:05 +0300

    ש

--- src/app/services/taking-delivery.service.ts
+++ src/app/services/taking-delivery.service.ts
@@ -2,56 +2,59 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AllOrder } from '../classes/AllOrder';
-import { Client } from '../classes/Client';
-import { GivingDelivery } from '../classes/GivingDelivery';
 import { TakingDelivery } from '../classes/TakingDelivery';
-import { ClientLoginComponent } from '../client-login/client-login.component';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TakingDeliveryService {
 
-  listTakingDelivery:Array<TakingDelivery>=new Array<TakingDelivery>();
-  
-  newTakingDelivery:AllOrder=new AllOrder();
-  // newTakingDelivery1:TakingDelivery=new TakingDelivery();
-    
+  listTakingDelivery: Array<TakingDelivery> = new Array<TakingDelivery>();
+  newTakingDelivery: AllOrder = new AllOrder();
+
   constructor(private http: HttpClient) { }
   url: string = "https://localhost:44337/api/TakingDelivery"
-  
-//שליפת רשימת לקיחת משלוח
-GatAllTakingDelivery():Observable<Array<TakingDelivery>>{
-  return this.http.get<Array<TakingDelivery>>(this.url+"/GatAllTakingDelivery")}
-          
-//שליפת לקיחת משלוח לפי קוד   
-GetAllOrder():Observable<AllOrder>{
-  return this.http.get<AllOrder>(this.url+"/GetAllOrder/"+this.newTakingDelivery.OrderID)}
 
-GetAddAllOrder(c:AllOrder):Observable<AllOrder>{
-  this.newTakingDelivery.OrderDate=new Date();
-  this.newTakingDelivery.FinalPay=1;
-  this.newTakingDelivery.Note="tgfhdfhdf";
-  return this.http.put<AllOrder>(this.url+"/GetAddAllOrder",c);
-}
-//שליפה של כל ההזמנה
-GetAllOrder1(id:number):Observable<Array<Array<AllOrder>>>{
- 
-  return this.http.get<Array<Array<AllOrder>>>(this.url+"/GetAllOrderByIdCompany/"+id);
-} 
-GetUpdatAllOrder(c:AllOrder):Observable<Array<AllOrder>>{
-  debugger
-  return this.http.post<Array<AllOrder>>(this.url+"/GetUpdatAllOrder",c);
-}
-//הוספת לקיחת משלוח 
-GetAddTakingDelivery(c:TakingDelivery):Observable<Array<TakingDelivery>>{
-  return this.http.put<Array<TakingDelivery>>(this.url+"/GetAddTakingDeliver",c)}
-        
-//עדכון לקיחת משלוח  
-GetUpdatTakingDelivery(c:TakingDelivery):Observable<Array<TakingDelivery>>{
-  return this.http.post<Array<TakingDelivery>>(this.url+"/GetUpdatTakingDelivery",c)}
-        
+  //שליפת רשימת לקיחת משלוח
+  GatAllTakingDelivery(): Observable<Array<TakingDelivery>> {
+    return this.http.get<Array<TakingDelivery>>(this.url + "/GatAllTakingDelivery")
+  }
+
+  //שליפת לקיחת משלוח לפי קוד   
+  GetAllOrder(): Observable<AllOrder> {
+    return this.http.get<AllOrder>(this.url + "/GetAllOrder/" + this.newTakingDelivery.OrderID)
+  }
+
+  GetAddAllOrder(c: AllOrder): Observable<AllOrder> {
+    this.newTakingDelivery.OrderDate = new Date();
+    this.newTakingDelivery.FinalPay = 1;
+    this.newTakingDelivery.Note = "tgfhdfhdf";
+    return this.http.put<AllOrder>(this.url + "/GetAddAllOrder", c);
+  }
+  //שליפה של רשימת ההזמנות מתוך רשימת המסלולים מתוך רשימת החברות
+  GetAllOrder1(id: number): Observable<Array<Array<AllOrder>>> {
+    return this.http.get<Array<Array<AllOrder>>>(this.url + "/GetAllOrderByIdCompany/" + id);
+  }
+
+  GetUpdatAllOrder(c: AllOrder): Observable<Array<AllOrder>> {
+    debugger
+    return this.http.post<Array<AllOrder>>(this.url + "/GetUpdatAllOrder", c);
+  }
+
+  //הוספת לקיחת משלוח 
+  GetAddTakingDelivery(c: TakingDelivery): Observable<Array<TakingDelivery>> {
+    return this.http.put<Array<TakingDelivery>>(this.url + "/GetAddTakingDeliver", c)
+  }
+
+  //עדכון לקיחת משלוח  
+  GetUpdatTakingDelivery(c: TakingDelivery): Observable<Array<TakingDelivery>> {
+    return this.http.post<Array<TakingDelivery>>(this.url + "/GetUpdatTakingDelivery", c)
+  }
+
   //מחיקת לקיחת משלוח  
-GetRemoveTakingDelivery(id:number):Observable<Array<TakingDelivery>>{
-  return this.http.delete<Array<TakingDelivery>>(this.url+"/GetRemoveTakingDelivery"+id)}
+  GetRemoveTakingDelivery(id: number): Observable<Array<TakingDelivery>> {
+    return this.http.delete<Array<TakingDelivery>>(this.url + "/GetRemoveTakingDelivery" + id)
+  }
 }
